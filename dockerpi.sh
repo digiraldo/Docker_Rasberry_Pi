697bacf0c135813003d9cc541d6e6a697acc8d34
digiraldo <disaned@hotmail.com>
2024-02-02 13:40:50 -0600

    Configurando que realice cambios en yaml

--- dockerpi.sh
+++ dockerpi.sh
@@ -88,10 +88,6 @@ Print_Style "Agregando Usuario $UserName al gruo docker..." "$GREEN"
 sleep 1s
 sudo gpasswd -a $UserName docker
 
-Print_Style "probando docker..." "$BLUE"
-sleep 1s
-sudo docker run hello-world
-
 sudo apt-get update
 
 Print_Style "INSTALACIÓN DE DOCKER-COMPOSE..." "$MAGENTA"
@@ -100,7 +96,7 @@ sleep 2s
 sudo apt-get update && sudo apt-get install -y docker-ce docker-compose
 
 Print_Style "==================================================================================" "$YELLOW"
-Disco=sudo lsblk -p -o NAME,SIZE,FSTYPE,LABEL,UUID,MOUNTPOINT -J | jq -r '.blockdevices[0] | .children[] | .name'
+Disco=$(sudo lsblk -p -o NAME,SIZE,FSTYPE,LABEL,UUID,MOUNTPOINT -J | jq -r '.blockdevices[0] | .children[] | .name')
 Print_Style "==================================================================================" "$YELLOW"
 
 Print_Style "MONTANDO DISCO EXTERNO..." "$RED"
@@ -108,12 +104,12 @@ sleep 2s
 sudo mkdir -p externo
 sudo mount $Disco externo
 #sudo mount /dev/sda1 /mnt/sda1
-
+sleep 1s
 Print_Style "==================================================================================" "$YELLOW"
-TZ=sudo cat /etc/timezone
-PUID=sudo id -u $UserName
-PGID=sudo id -g $UserName
-DiscoExterno=sudo lsblk -o NAME,SIZE,FSTYPE,LABEL,UUID,MOUNTPOINT -J | jq -r '.blockdevices[0] | .children[] | .mountpoint'
+TZ=$(sudo cat /etc/timezone)
+PUID=$(sudo id -u $UserName)
+PGID=$(sudo id -g $UserName)
+DiscoExterno=$(sudo lsblk -o NAME,SIZE,FSTYPE,LABEL,UUID,MOUNTPOINT -J | jq -r '.blockdevices[0] | .children[] | .mountpoint')
 Print_Style "==================================================================================" "$YELLOW"
 
 cd ~
@@ -122,12 +118,32 @@ cd ~
 echo "Tomando docker-compose.yaml del repositorio..."
 curl -H "Accept-Encoding: identity" -L -o docker-compose.yaml https://raw.githubusercontent.com/digiraldo/Docker_Rasberry_Pi/main/docker-compose.yaml
 chmod +x docker-compose.yaml
-sudo sed -i "s:usuarioid:- PUID=$PUID:g" docker-compose.yaml
-sudo sed -i "s:grupoid:- PGID=$PGID:g" docker-compose.yaml
-sudo sed -i "s:timezona:- TZ=$TZ:g" docker-compose.yaml
+
+sleep 1s
+Print_Style "Configurando Id de Usuario PUID=$PUID ..." "$CYAN"
+sudo sed -i "s:usuarioid:$PUID:g" docker-compose.yaml
+
+sleep 1s
+Print_Style "Configurando Id de Grupo PGID=$PGID ..." "$CYAN"
+sudo sed -i "s:grupoid:$PGID:g" docker-compose.yaml
+
+sleep 1s
+Print_Style "Configurando Zona Horaria TZ=$TZ ..." "$CYAN"
+sudo sed -i "s:timezona:$TZ:g" docker-compose.yaml
+
+sleep 1s
+Print_Style "Configurando Nombre de Usuario a: $UserName ..." "$CYAN"
 sudo sed -i "s:usernaa:$UserName:g" docker-compose.yaml
+
+sleep 1s
+Print_Style "Configurando Disco Externo $DiscoExterno ..." "$CYAN"
 #sudo sed -i "s:discc:$Disco:g" docker-compose.yaml
-sudo sed -i "s:discomontadoext:- $DiscoExterno:g" docker-compose.yaml
+sudo sed -i "s:discomontadoext:$DiscoExterno:g" docker-compose.yaml
+sleep 1s
+
+Print_Style "mostrando cambios en docker-compose.yaml..." "$BLUE"
+sudo cat docker-compose.yaml
+sleep 3s
 
 Print_Style "probando docker-compose..." "$BLUE"
 sleep 1s
