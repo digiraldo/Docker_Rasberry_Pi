a7f4328b6061db960a005330c6d20effe080c891
digiraldo <disaned@hotmail.com>
2024-02-09 18:40:40 -0600

    config

--- dck.sh
+++ dck.sh
@@ -109,11 +109,10 @@ sleep 2s
 cd ~
 
 
-
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 echo "deb [arch=armhf] https://download.docker.com/linux/debian \
-     $(lsb_release -cs) stable" | \
+    $(lsb_release -cs) stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update && sudo apt-get install -y --no-install-recommends docker-ce docker-compose
 sleep 2s
