53a72b4ad69aaaa40362f573f5874e991aee2a3d
digiraldo <disaned@hotmail.com>
2024-02-14 19:50:48 -0600

    dc install

--- dockerpi.sh
+++ dockerpi.sh
@@ -239,7 +239,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 Executing docker install script, commit: 7cae5f8b0decc17d6571f9f52eb840fbc13b2737
 
-# sudo apt-get update && sudo apt-get install -y --no-install-recommends docker-ce docker-compose
+sudo apt-get update && sudo apt-get install -y --no-install-recommends docker-ce docker-compose
 
 echo "========================================================================="
 sudo docker --version
